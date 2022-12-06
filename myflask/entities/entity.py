4c2a668971cc88011c5ad17ae0b35664d96cd0a9
min <min@naver.com>
2022-12-06 10:41:48 +0900

    initial commit

---
+++ myflask/entities/entity.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+from datetime import datetime
+from sqlalchemy import create_engine, Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import os
+db_url = os.environ['FLASK_DB_URL']
+db_name = os.environ['FLASK_DB_NAME']
+db_user = os.environ['FLASK_DB_USER']
+db_password = os.environ['FLASK_DB_PASSWORD']
+
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+class Entity():
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    last_updated_by = Column(String)
+
+    def __init__(self, created_by):
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        self.last_updated_by = created_by
